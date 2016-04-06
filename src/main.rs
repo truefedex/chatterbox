8f926a99ba937196da3b147a02386fe4b876418d
Pheodor Tsapana <truefedex@gmail.com>
2016-04-07 00:43:24 +0300

    tabs->spaces

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use std::env;
 use hound::{ WavWriter, WavSpec };
 
 struct WavChatterboxOutput {
-	writer: WavWriter<std::io::BufWriter<std::fs::File>>,
+    writer: WavWriter<std::io::BufWriter<std::fs::File>>,
 }
 
 impl WavChatterboxOutput {
     fn new(file_name : &str) -> WavChatterboxOutput {
-		let spec = WavSpec {
-			channels: 1,
-			sample_rate: 44100,
-			bits_per_sample: 16
+        let spec = WavSpec {
+            channels: 1,
+            sample_rate: 44100,
+            bits_per_sample: 16
         };
         let writer : WavWriter<std::io::BufWriter<std::fs::File>> = WavWriter::create(file_name, spec).unwrap();
         WavChatterboxOutput { writer: writer, }
@@ -21,20 +21,20 @@ impl WavChatterboxOutput {
 }
 
 impl chatterbox::Output for WavChatterboxOutput {
-	fn write_sample(&mut self, sample: i16) {
-		self.writer.write_sample(sample).unwrap();
-	}		
+    fn write_sample(&mut self, sample: i16) {
+        self.writer.write_sample(sample).unwrap();
+    }       
 }
 
-fn main() {	
+fn main() { 
     let args: Vec<_> = env::args().collect();
     if args.len() <= 1 {
         println!("Chatterbox v 0.1.0\nИспользование:\n  chatterbox <текст>");
         return;
     } else {
-		println!("The first argument is {}", args[1]);
-	}
-	
-	let backend : &mut chatterbox::Backend = &mut chatterbox::backends::Synthetic{ x: 0.0 } as &mut chatterbox::Backend;
-	backend.synth("test", &mut WavChatterboxOutput::new("output.wav") as &mut chatterbox::Output);
+        println!("The first argument is {}", args[1]);
+    }
+    
+    let backend : &mut chatterbox::Backend = &mut chatterbox::backends::Synthetic{ x: 0.0 } as &mut chatterbox::Backend;
+    backend.synth("test", &mut WavChatterboxOutput::new("output.wav") as &mut chatterbox::Output);
 }
